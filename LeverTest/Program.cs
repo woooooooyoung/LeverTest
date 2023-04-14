56f75f1999d495ec11dc0cf4e45ea607e8de16ff
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-14 20:14:39 +0900

    초기 커밋.

---
+++ LeverTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeverTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
---
+++ git Test1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace git_Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
