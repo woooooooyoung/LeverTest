47c7352dbc8ed88714fa54b47f53158e4a8b8b0d
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-14 20:16:40 +0900

    00.LeverTest 1
    문장을 입력 받은 후, 단어를 입력 받아 문장 중 단어가 시작하는 위치 출력(없을 경우 -1 출력)

--- LeverTest/Program.cs
+++ LeverTest/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
+            // 문자열 속 문자 찾기
+            Console.WriteLine("문장과 문자를 입력하세요.");    // 문장과 문자를 입력하세요. 출력
+            string str = Console.ReadLine();                   // 입력한 문장을 str에 저장
+            string str1 = Console.ReadLine();                  // 입력한 문자를 str1에 저장
+            Console.WriteLine(str.IndexOf(str1, 0, str.Length)); // str에 저장된 문장중 str1의 처음 문자를 찾을 수 있음
         }
     }
 }
